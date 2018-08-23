2a02d98b3359b8dcd98a5a5b32a189fe2a11b8b7
Your Name <darwin4416@gmail.com>
2018-08-23 17:15:20 +0530

    added select planet div

--- src/app/destinations/destinations.component.ts
+++ src/app/destinations/destinations.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChange, SimpleChanges, OnChanges } from '@angular/core';
 
 @Component({
     selector: 'destinations',
     templateUrl: './destinations.component.html',
     styleUrls: ['./destinations.component.css']
 })
-export class DestinationsComponent implements OnInit {
+export class DestinationsComponent implements OnInit,OnChanges {
     showPlanet1: boolean = false;
     showPlanet2: boolean = false;
     showPlanet3: boolean = false;
     showPlanet4: boolean = false;
+
+    planetCopy;
+    isDisabled = false;
     @Input() planets;
     @Input() vehicles;
     constructor() { }
@@ -17,13 +20,22 @@ export class DestinationsComponent implements OnInit {
     ngOnInit() {
       console.log("here",this.vehicles)
     }
-    onPlanetSelect(planet) {
-        if(planet ==1 ){
-            this.showPlanet1 = !this.showPlanet1;
+    ngOnChanges(changes:SimpleChanges){
+        if(changes.planets){
+            this.planetCopy = this.planets
         }
-        else if(planet == 2){
-            this.showPlanet2 = !this.showPlanet2;
-        }
+    }
+    // onPlanetSelect(planet) {
+    //     if(planet ==1 ){
+    //         this.showPlanet1 = !this.showPlanet1;
+    //     }
+    //     else if(planet == 2){
+    //         this.showPlanet2 = !this.showPlanet2;
+    //     }
         
+    // }
+    onPlanetSelect(e){
+     let index= e.target.value;
+     this.isDisabled = true;
     }
 }
